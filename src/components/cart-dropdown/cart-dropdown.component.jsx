aa38bf9dfbbfeea9770cc8966921cd16eb593ac9
Costinnn <crauciuc.costin@yahoo.com>
2022-10-16 16:52:59 +0300

    Checkout page, checkout cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,27 @@
-import './cart-dropdown.styles.scss'
-import Button from '../button/button.component'
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../../contexts/cart.context";
+import "./cart-dropdown.styles.scss";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
-const CartDropdown = () =>{
-    return(
-        <div className='cart-dropdown-container'>
-            <div cart-items></div>
-            <Button>GO TO CHECKOUT</Button>
-        </div>
-    );
-}
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () =>{
+    navigate('/checkout')
+  }
+
+  return (
+    <div className="cart-dropdown-container">
+      <div className="cart-items">
+        {cartItems.map((arrItem) => (
+          <CartItem key={arrItem.id} cartItem={arrItem} />
+        ))}
+      </div>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+    </div>
+  );
+};
 
 export default CartDropdown;
